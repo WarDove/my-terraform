a4429a38faf93d1b6b6504ea07ea7dcc999654e6
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-08-21 20:23:25 +0400

    added azin-sphere

---
+++ azin-sphere/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.2.6"
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+
+
