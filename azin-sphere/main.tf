b2a0b19a352955060e220ce99254f6a4821f074d
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-08-25 17:42:39 +0400

    updated

--- azin-sphere/main.tf
+++ azin-sphere/main.tf
@@ -18,24 +18,54 @@ resource "vsphere_host" "esx-01" {
 
 }
 
-# Add Compute Cluster
-resource "vsphere_compute_cluster" "azin-sphere-cc" {
-  name            = "azin-sphere compute cluster"
-  datacenter_id   = vsphere_datacenter.azin-sphere-dc.moid
-  host_system_ids = [vsphere_host.esx-01.id]
+# # Add Compute Cluster
+# resource "vsphere_compute_cluster" "azin-sphere-cc" {
+#   name            = "azin-sphere-compute-cluster"
+#   datacenter_id   = vsphere_datacenter.azin-sphere-dc.moid
+#   host_system_ids = [vsphere_host.esx-01.id]
 
-  drs_enabled = false
-  ha_enabled  = false
+#   drs_enabled = false
+#   ha_enabled  = false
+# }
+
+# # Add Resource Pool if DRS & HA Enabled 
+#  resource "vsphere_resource_pool" "azin-sphere-rp" {
+#    name                    = "azin-sphere-rp"
+#    parent_resource_pool_id = vsphere_compute_cluster.azin-sphere-cc.resource_pool_id
+#  }
+
+
+# Get Datastore info from vcenter
+data "vsphere_datastore" "main-datastore" {
+  name          = "main-datastore"
+  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
 }
 
-# #Add Resource pool
-# resource "vsphere_resource_pool" "azin-sphere-rp" {
-#   name                    = "azin-sphere-rp"
-#   parent_resource_pool_id = vsphere_compute_cluster.azin-sphere-cc.resource_pool_id
-# }
+# Get Network info from vcenter
+data "vsphere_network" "vm-network" {
+  name          = "VLAN3513"
+  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+}
 
-# resource "vsphere_folder" "folder" {
-#   path          = "consul"
-#   type          = "vm"
-#   datacenter_id = data.vsphere_datacenter.azin-sphere-dc.id
-# }
+# Target Resource Pool
+data "vsphere_resource_pool" "target-resource-pool" {
+  name          = "${var.esx-01_server}/Resources/"
+  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+}
+
+# Create VM
+resource "vsphere_virtual_machine" "test" {
+  name             = "test"
+  resource_pool_id = data.vsphere_resource_pool.target-resource-pool.id 
+  datastore_id     = data.vsphere_datastore.main-datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "centos7_64Guest"
+  network_interface {
+    network_id = data.vsphere_network.vm-network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
