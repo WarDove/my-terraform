61557a3e7cfd3c867b336ba55881d633125dc101
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-08-25 19:32:49 +0400

    updated

--- azin-sphere/main.tf
+++ azin-sphere/main.tf
@@ -39,33 +39,42 @@ resource "vsphere_host" "esx-01" {
 data "vsphere_datastore" "main-datastore" {
   name          = "main-datastore"
   datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+  depends_on = [
+    vsphere_host.esx-01
+  ]
 }
 
 # Get Network info from vcenter
 data "vsphere_network" "vm-network" {
   name          = "VLAN3513"
   datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+  depends_on = [
+    vsphere_host.esx-01
+  ]
 }
 
 # Target Resource Pool
 data "vsphere_resource_pool" "target-resource-pool" {
   name          = "${var.esx-01_server}/Resources/"
   datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+  depends_on = [
+    vsphere_host.esx-01
+  ]
 }
 
-# Create VM
-resource "vsphere_virtual_machine" "test" {
-  name             = "test"
-  resource_pool_id = data.vsphere_resource_pool.target-resource-pool.id 
-  datastore_id     = data.vsphere_datastore.main-datastore.id
-  num_cpus         = 1
-  memory           = 1024
-  guest_id         = "centos7_64Guest"
-  network_interface {
-    network_id = data.vsphere_network.vm-network.id
-  }
-  disk {
-    label = "disk0"
-    size  = 20
-  }
-}
+# # Create VM
+# resource "vsphere_virtual_machine" "test" {
+#   name             = "test"
+#   resource_pool_id = data.vsphere_resource_pool.target-resource-pool.id 
+#   datastore_id     = data.vsphere_datastore.main-datastore.id
+#   num_cpus         = 1
+#   memory           = 1024
+#   guest_id         = "centos7_64Guest"
+#   network_interface {
+#     network_id = data.vsphere_network.vm-network.id
+#   }
+#   disk {
+#     label = "disk0"
+#     size  = 20
+#   }
+# }
